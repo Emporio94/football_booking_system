a861b8ab33aa1b1f755a3ce300e05b125541fc6b
Abdisalam Abukar <2618966a@student.gla.ac.uk>
2024-09-16 02:02:24 +0100

    Datetime plus html templates

--- football_app/players/views.py
+++ football_app/players/views.py
@@ -1,13 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpRequest, Http404
+from datetime import datetime
+
 
 # Create your views here.
 
 def list_page(request):
-    return HttpResponse("You are now on the list_page review")
+    
+    current_time = datetime.now()
+
+    context_dict = {'boldmessage': "This is my message",
+                    'current_time': current_time}
+    
+    return render(request, 'players/player_list.html', context_dict)
 
 def login(request):
-    return HttpResponse("You are now in the login view")
+    return render(request, )
 
 def signup(request):
     return HttpResponse("You are now in the sign in view")
